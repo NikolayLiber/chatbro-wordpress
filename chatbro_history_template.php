927933e6ddaadfd4da3a093b35f9205966258d39
Nikolay Liber <fwnd80@gmail.com>
2016-12-14 17:26:44 +0500

    History fixed

--- chatbro_history_template.php
+++ chatbro_history_template.php
@@ -1,4 +1,29 @@
-<?php get_header(); ?>
+<?php
+
+
+// require_once('util.php');
+// require_once('plugin.php');
+
+// get_header();
+
+function get_opt() {
+    global $wp;
+    $guid = ChatBroUtils::get_option(ChatBroPlugin::guid_setting);
+
+    $current_url_param = $wp->query_string;
+    $param = array(
+        'encodedChatGuid' =>       md5($guid),
+        'containerDivId' => "div_chatbro_history",
+        'currentUrlParam'=>     $current_url_param,
+        'useStandardHistoryWidgetSettings' => true,
+        'chatPaginatorUrlPrefix' => get_permalink()
+
+    );
+    $json_param = json_encode($param);
+    return $json_param;
+}
+
+?>
 
 <div id="div_chatbro_history" class="content-area">
 
@@ -6,33 +31,27 @@
 <script>
 /* Chatbro Widget Embed Code Start*/
 function ChatbroLoader(chats, async) {
-    async = async || true;
+    async = async !== false;
     var params = {
         embedChatsParameters: chats instanceof Array ? chats : [chats],
-        needLoadCode: typeof Chatbro === "undefined"
+        needLoadCode: typeof Chatbro === 'undefined'
     };
     var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
     xhr.onload = function () {
         eval(xhr.responseText);
     };
     xhr.onerror = function () {
-        console.error("Chatbro loading error");
+        console.error('Chatbro loading error');
     };
-    xhr.open("POST", "http://www.chatbro.com/embed_chats", async);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send("parameters=" + encodeURIComponent(JSON.stringify(params)));
+    xhr.open('POST', '//www.chatbro.com/embed_chats/', async);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('parameters=' + encodeURIComponent(JSON.stringify(params)));
 }
 /* Chatbro Widget Embed Code End*/
 
-var p = <?php echo $o = get_opt() ?>;
-var param={
-    chatPath: p.chatPath,
-    	containerDivId: p.containerDivId,
-    	chatLanguage: p.chatLanguage,
-    	useStandardHistoryWidgetSettings: true,
-        chatPaginatorUrlPrefix: '<?php the_permalink() ?>'
-	};
-var batchId = /page=(.*)&/i.exec(p.currentUrlParam);
+var param = <?php echo get_opt(); ?>;
+var batchId = /page=(.*)&/i.exec(param.currentUrlParam);
 if(batchId != null)
     param.batchId=batchId[1];
 
