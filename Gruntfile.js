6411e617aec89d043f5e37d798b8a5d4aefdbc40
Nikolay Liber <fwnd80@gmail.com>
2017-01-31 22:11:20 +0500

    Moved to Standard code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,205 +1,227 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-		bower: {
-			install: {
-				options: {
-					copy: false
-				}
-			}
-		},
+module.exports = function (grunt) {
+  grunt.initConfig({
+    bower: {
+      install: {
+        options: {
+          copy: false
+        }
+      }
+    },
 
-		bower_concat: {
-			css: {
-				dest: {
-					css: '_build/css/lib.css'
-				},
+    bower_concat: {
+      css: {
+        dest: {
+          css: '_build/css/lib.css'
+        },
 
-				mainFiles: {
-					bootstrap: 'dist/css/bootstrap.css',
-					'font-awesome': 'css/font-awesome.css'
-				}
+        mainFiles: {
+          bootstrap: 'dist/css/bootstrap.css',
+          'font-awesome': 'css/font-awesome.css'
+        }
+      },
 
-			},
+      js: {
+        dest: {
+          js: '_build/js/lib.js'
+        },
 
-			js: {
-				dest: {
-					js: '_build/js/lib.js',
-				},
+        mainFiles: {
+          bootstrap: 'dist/js/bootstrap.js'
+        }
+      }
+    },
 
-				mainFiles: {
-					bootstrap: 'dist/js/bootstrap.js'
-				}
-			}
-		},
+    uglify: {
+      all: {
+        files: {
+          'js/chatbro.min.js': ['_build/js/lib.js', 'src/js/chatbro.admin.js']
+        }
+      }
+    },
 
-		uglify: {
-			all: {
-				files: {
-					'js/chatbro.min.js': ['_build/js/lib.js', 'src/js/chatbro.admin.js']
-				}
-			}
-		},
+    cssmin: {
+      all: {
+        files: {
+          'css/chatbro.min.css': ['_build/css/lib.css', '_build/css/chatbro.css']
+        }
+      }
+    },
 
-		cssmin: {
-			all: {
-				files: {
-					'css/chatbro.min.css': ['_build/css/lib.css', '_build/css/chatbro.css']
-				}
-			}
-		},
+    pkg: grunt.file.readJSON('package.json'),
+    po2mo: {
+      files: {
+        src: [
+          'languages/*.po',
+          '!node_modules/**'
+        ],
+        expand: true
+      }
+    },
 
-		pkg: grunt.file.readJSON('package.json'),
-		po2mo: {
-		    files: {
-		        src: [ 'languages/*.po',
-		        	   '!node_modules/**' ],
-		        expand: true,
-		    },
-		},
+    pot: {
+      options: {
+        text_domain: 'chatbro-plugin', // Your text domain. Produces my-text-domain.pot
+        dest: 'languages/', // directory to place the pot file
+        keywords: [ // WordPress localisation functions
+          '__:1',
+          '_e:1',
+          '_x:1,2c',
+          'esc_html__:1',
+          'esc_html_e:1',
+          'esc_html_x:1,2c',
+          'esc_attr__:1',
+          'esc_attr_e:1',
+          'esc_attr_x:1,2c',
+          '_ex:1,2c',
+          '_n:1,2',
+          '_nx:1,2,4c',
+          '_n_noop:1,2',
+          '_nx_noop:1,2,3c'
+        ],
+        msgmerge: true,
+        comment_tag: 'Translators:'
+      },
+      files: {
+        src: [
+          '**/*.php',
+          '!node_modules/**'
+        ], // Parse all php files
+        expand: true
+      }
+    },
 
-		pot: {
-			options:{
-				text_domain: 'chatbro-plugin', //Your text domain. Produces my-text-domain.pot
-				dest: 'languages/', //directory to place the pot file
-				keywords: [ //WordPress localisation functions
-					'__:1',
-					'_e:1',
-					'_x:1,2c',
-					'esc_html__:1',
-					'esc_html_e:1',
-					'esc_html_x:1,2c',
-					'esc_attr__:1',
-					'esc_attr_e:1',
-					'esc_attr_x:1,2c',
-					'_ex:1,2c',
-					'_n:1,2',
-					'_nx:1,2,4c',
-					'_n_noop:1,2',
-					'_nx_noop:1,2,3c'
-				],
-				msgmerge: true,
-				comment_tag: "Translators:"
-			},
-			files:{
-				src:  [ '**/*.php',
-						'!node_modules/**' ], //Parse all php files
-				expand: true,
-			}
-		},
+    checktextdomain: {
+      options: {
+        text_domain: 'chatbro-plugin',
+        correct_domain: true, // Will correct missing/variable domains
+        keywords: [ // WordPress localisation functions
+          '__:1,2d',
+          '_e:1,2d',
+          '_x:1,2c,3d',
+          'esc_html__:1,2d',
+          'esc_html_e:1,2d',
+          'esc_html_x:1,2c,3d',
+          'esc_attr__:1,2d',
+          'esc_attr_e:1,2d',
+          'esc_attr_x:1,2c,3d',
+          '_ex:1,2c,3d',
+          '_n:1,2,4d',
+          '_nx:1,2,4c,5d',
+          '_n_noop:1,2,3d',
+          '_nx_noop:1,2,3c,4d'
+        ]
+      },
+      files: {
+        src: [
+          '**/*.php',
+          '!node_modules/**'
+        ], // All php files
+        expand: true
+      }
+    },
 
-		checktextdomain: {
-			options:{
-				text_domain: 'chatbro-plugin',
-				correct_domain: true, //Will correct missing/variable domains
-				keywords: [ //WordPress localisation functions
-				    '__:1,2d',
-				    '_e:1,2d',
-				    '_x:1,2c,3d',
-				    'esc_html__:1,2d',
-				    'esc_html_e:1,2d',
-				    'esc_html_x:1,2c,3d',
-				    'esc_attr__:1,2d',
-				    'esc_attr_e:1,2d',
-				    'esc_attr_x:1,2c,3d',
-				    '_ex:1,2c,3d',
-				    '_n:1,2,4d',
-				    '_nx:1,2,4c,5d',
-				    '_n_noop:1,2,3d',
-				    '_nx_noop:1,2,3c,4d'
-				],
-			},
-			files: {
-			src:  [ '**/*.php',
-					'!node_modules/**' ], //All php files
-			expand: true,
-			},
-		},
+    svn_fetch: {
+      options: {
+        repository: 'https://plugins.svn.wordpress.org/chatbro/',
+        path: './'
+      },
+      svn: {
+        map: { 'dist': 'trunk' }
+      }
+    },
 
-		svn_fetch: {
-			options: {
-				repository: 'https://plugins.svn.wordpress.org/chatbro/',
-				path: './',
-				svnOptions: {
-					username: 'yozeg',
-					password: 'W*ojrKFw@73V'
-				}
-			},
-			svn: {
-				map: { 'svn': 'trunk' }
-			}
-		},
+    sync: {
+      main: {
+        files: [{
+          src: [
+            '**/*.php',
+            'js/**/*.js',
+            'css/**/*.css',
+            'languages/**/*.mo',
+            'favicon_small.png',
+            'fonts/**/*',
+            '!LICENSE.txt',
+            '!node_modules/**',
+            '!dist/**',
+            '!src/**',
+            '!bower_components/**',
+            '!_build/**',
+            '!.svn/**',
+            '!readme.txt'
+          ],
+          dest: 'dist/'
+        }],
+        verbose: true,
+        failOnError: true,
+        updateAndDelete: true,
+        compareUsing: 'md5',
+        ignoreInDest: ['LICENCE.txt', '.svn/**']
+      }
+    },
 
-		sync: {
-			main: {
-				files: [
-					{ src: ['**/*.php', '**/*.js', '**/*.css', '**/*.mo', '!Gruntfile.js', '!node_modules/**', '!svn/**'],  dest: 'svn/' }
-				],
-				verbose: true,
-				pretend: true,
-				failOnError: true,
-				updateAndDelete: true,
-				compareUsing: "md5"
-			},
-		},
+    clean: {
+      all: ['_build', 'bower_components', 'js', 'css', 'fonts', 'dist'],
+      build: ['_build', 'js', 'css', 'fonts'],
+      release: ['dist']
+    },
 
-		clean: ['_build', 'bower_components', 'js', 'css', 'fonts'],
+    copy: {
+      fonts: {
+        expand: true,
+        src: ['bower_components/font-awesome/fonts/*', 'bower_components/bootstrap/fonts/*'],
+        dest: 'fonts/',
+        filter: 'isFile',
+        flatten: true
+      }
+    },
 
-		copy: {
-			fonts: {
-				expand: true,
-				src: ['bower_components/font-awesome/fonts/*', 'bower_components/bootstrap/fonts/*'],
-				dest: 'fonts/',
-				filter: 'isFile',
-				flatten: true
-			}
-		},
+    concat: {
+      devjs: {
+        src: ['_build/js/lib.js', 'src/js/*.js'],
+        dest: 'js/chatbro.min.js'
+      },
 
-		concat: {
-			devjs: {
-				src: ['_build/js/lib.js', 'src/js/*.js'],
-				dest: 'js/chatbro.min.js'
-			},
+      devcss: {
+        src: ['_build/css/lib.css', '_build/css/chatbro.css'],
+        dest: 'css/chatbro.min.css'
+      }
+    },
 
-			devcss: {
-				src: ['_build/css/lib.css', '_build/css/chatbro.css'],
-				dest: 'css/chatbro.min.css'
-			}
-		},
+    sass: {
+      dist: {
+        files: {
+          '_build/css/chatbro.css': 'src/scss/chatbro.scss'
+        }
+      }
+    },
 
-		sass: {
-			dist: {
-				files: {
-					'_build/css/chatbro.css' : 'src/scss/chatbro.scss'
-				}
-			}
-		},
+    eslint: {
+      target: 'src/js/**.js'
+    }
+  })
 
-		eslint: {
-			target: 'src/js/**.js'
-		}
-	});
+  grunt.loadNpmTasks('grunt-bower-task')
+  grunt.loadNpmTasks('grunt-bower-concat')
+  grunt.loadNpmTasks('grunt-po2mo')
+  grunt.loadNpmTasks('grunt-pot')
+  grunt.loadNpmTasks('grunt-checktextdomain')
+  grunt.loadNpmTasks('grunt-svn-fetch')
+  grunt.loadNpmTasks('grunt-sync')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-sass')
+  grunt.loadNpmTasks('grunt-eslint')
 
-	grunt.loadNpmTasks('grunt-bower-task');
-	grunt.loadNpmTasks('grunt-bower-concat');
-	grunt.loadNpmTasks('grunt-po2mo');
-	grunt.loadNpmTasks('grunt-pot');
-	grunt.loadNpmTasks('grunt-checktextdomain');
-	grunt.loadNpmTasks('grunt-svn-fetch');
-	grunt.loadNpmTasks('grunt-sync');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-eslint');
-
-	grunt.registerTask('default', ['bower', 'bower_concat:js', 'bower_concat:css', 'uglify', 'sass', 'cssmin', 'copy:fonts']);
-	grunt.registerTask('compact', ["cssmin", "uglify"]);
-	grunt.registerTask('build:css:prod', ['bower_concat:css', 'sass', 'cssmin']);
-	grunt.registerTask('build:css:dev', ['bower_concat:css', 'sass', 'concat:devcss']);
-	grunt.registerTask('build:js:prod', ['bower_concat:js', 'eslint', 'uglify']);
-	grunt.registerTask('build:js:dev', ['bower_concat:js', 'eslint', 'concat:devjs']);
-	grunt.registerTask('build:prod', ['build:js:prod', 'build:css:prod']);
-	grunt.registerTask('build:dev', ['build:js:dev', 'build:css:dev']);
+  grunt.registerTask('default', ['bower', 'bower_concat:js', 'bower_concat:css', 'uglify', 'sass', 'cssmin', 'copy:fonts'])
+  grunt.registerTask('compact', ['cssmin', 'uglify'])
+  grunt.registerTask('build:css:prod', ['bower_concat:css', 'sass', 'cssmin'])
+  grunt.registerTask('build:css:dev', ['bower_concat:css', 'sass', 'concat:devcss'])
+  grunt.registerTask('build:js:prod', ['bower_concat:js', 'eslint', 'uglify'])
+  grunt.registerTask('build:js:dev', ['bower_concat:js', 'eslint', 'concat:devjs'])
+  grunt.registerTask('build:prod', ['build:js:prod', 'build:css:prod', 'copy:fonts'])
+  grunt.registerTask('build:dev', ['build:js:dev', 'build:css:dev', 'copy:fonts'])
+  grunt.registerTask('release', ['clean:build', 'clean:release', 'build:prod', 'svn_fetch', 'sync'])
 }
--- src/js/chatbro.admin.js
+++ src/js/chatbro.admin.js
@@ -1,115 +1,114 @@
-jQuery(document).ready(function($) {
-	var t = ['except_listed', 'only_listed'];
-	var oldGuid;
+jQuery(document).ready(function ($) {
+  var t = ['except_listed', 'only_listed']
+  var oldGuid
 
-	$('#chatbro_chat_display').change(function() {
-		if (t.indexOf($('#chatbro_chat_display').val()) != -1)
-			$('#chatbro_chat_selected_pages-group').show();
-		else
-			$('#chatbro_chat_selected_pages-group').hide();
-	});
+  $('#chatbro_chat_display').change(function () {
+    if (t.indexOf($('#chatbro_chat_display').val()) !== -1) {
+      $('#chatbro_chat_selected_pages-group').show()
+    } else {
+      $('#chatbro_chat_selected_pages-group').hide()
+    }
+  })
 
-	$('#chatbro_chat_display').change();
+  $('#chatbro_chat_display').change()
 
-	$('#settings-tabs a').click(function(e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
+  $('#settings-tabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
 
-	$('#chatbro-settings-form').ajaxForm({
-		url: ajaxurl,
-		type: 'POST',
+  $('#chatbro-settings-form').ajaxForm({
+    url: ajaxurl,
+    type: 'POST',
 
-		success: function(response) {
-			if (response == '0') {
-				window.location = $('#chb-login-url').val();
-				return;
-			}
+    success: function (response) {
+      if (response === '0') {
+        window.location = $('#chb-login-url').val()
+        return
+      }
 
-			response = JSON.parse(response);
+      response = JSON.parse(response)
 
-			var msgDiv = $('#chatbro-message');
-			msgDiv.removeClass();
-			msgDiv.html('');
+      var msgDiv = $('#chatbro-message')
+      msgDiv.removeClass()
+      msgDiv.html('')
 
-			if (response.hasOwnProperty('message')) {
-				if (response.hasOwnProperty('msg_type') && response['msg_type'] == 'error') {
-					msgDiv.addClass('bs-callout-small bs-callout-small-danger');
-				}
-				else {
-					msgDiv.addClass('bs-callout-small bs-callout-small-success');
-				}
+      if (response.hasOwnProperty('message')) {
+        if (response.hasOwnProperty('msg_type') && response['msg_type'] === 'error') {
+          msgDiv.addClass('bs-callout-small bs-callout-small-danger')
+        } else {
+          msgDiv.addClass('bs-callout-small bs-callout-small-success')
+        }
 
-				msgDiv.html(response['message']);
-			}
+        msgDiv.html(response['message'])
+      }
 
-			$('.with-errors').empty();
-			$('.form-group').removeClass('has-error has-success');
+      $('.with-errors').empty()
+      $('.form-group').removeClass('has-error has-success')
 
-			if (response.hasOwnProperty('field_messages')) {
-				var focused = false;
+      if (response.hasOwnProperty('field_messages')) {
+        var focused = false
 
-				Object.keys(response.field_messages).forEach(function(id) {
-					var m = response.field_messages[id];
-					var group = $('#' + id + '-group');
+        Object.keys(response.field_messages).forEach(function (id) {
+          var m = response.field_messages[id]
+          var group = $('#' + id + '-group')
 
-					$('.with-errors', group).html('<ul class="list-unstyled"><li>' + m.message + '</li></ul>');
+          $('.with-errors', group).html('<ul class="list-unstyled"><li>' + m.message + '</li></ul>')
 
-					if (m.type == 'error') {
-						group.addClass('has-error');
+          if (m.type === 'error') {
+            group.addClass('has-error')
 
-						if (!focused) {
-							$('#' + id).focus();
-							focused = true;
-						}
-					}
-				});
-			}
+            if (!focused) {
+              $('#' + id).focus()
+              focused = true
+            }
+          }
+        })
+      }
 
-			enableSubmit();
-		}
-	});
+      enableSubmit()
+    }
+  })
 
-	function disableSubmit() {
-		$('#chatbro-save').button('saving').addClass('disabled').blur();
-	}
+  function disableSubmit () {
+    $('#chatbro-save').button('saving').addClass('disabled').blur()
+  }
 
-	function enableSubmit() {
-		$('#chatbro-save').button('reset').removeClass('disabled');
-	}
+  function enableSubmit () {
+    $('#chatbro-save').button('reset').removeClass('disabled')
+  }
 
-	$('#chatbro-save').click(function() {
-		disableSubmit();
+  $('#chatbro-save').click(function () {
+    disableSubmit()
 
-		oldGuid = $('#chb-sec-key').val();
-		var newGuid = $('#chatbro_chat_guid').val();
+    oldGuid = $('#chb-sec-key').val()
+    var newGuid = $('#chatbro_chat_guid').val()
 
-		if (oldGuid != newGuid) {
-			$('#chb-old-key span').html(oldGuid);
-			$('#chb-confirm-guid-modal').modal('show');
-			return;
-		}
+    if (oldGuid !== newGuid) {
+      $('#chb-old-key span').html(oldGuid)
+      $('#chb-confirm-guid-modal').modal('show')
+      return
+    }
 
-		$('#chatbro-settings-form').submit();
-	});
+    $('#chatbro-settings-form').submit()
+  })
 
-	$('#chb-confirm-guid-modal').on('hidden.bs.modal', function() {
-		if ($(this).hasClass('submit-confirmed')) {
-			$(this).removeClass('submit-confirmed');
-			$('#chatbro-settings-form').submit();
-		}
-		else {
-			$('#chatbro_chat_guid').val(oldGuid);
-			enableSubmit();
-		}
-	});
+  $('#chb-confirm-guid-modal').on('hidden.bs.modal', function () {
+    if ($(this).hasClass('submit-confirmed')) {
+      $(this).removeClass('submit-confirmed')
+      $('#chatbro-settings-form').submit()
+    } else {
+      $('#chatbro_chat_guid').val(oldGuid)
+      enableSubmit()
+    }
+  })
 
-	$('#chb-confirm-guid-modal button.btn-primary').click(function() {
-		$('#chb-confirm-guid-modal').addClass('submit-confirmed').modal('hide');
-	});
+  $('#chb-confirm-guid-modal button.btn-primary').click(function () {
+    $('#chb-confirm-guid-modal').addClass('submit-confirmed').modal('hide')
+  })
 
-	// Modal div must not be inside any element with relative or fixed postition
-	// or it will be shown behind it's backdrop. Let's move it to the top level of the body.
-	var modal = $('#chb-confirm-guid-modal');
-	$('body').append(modal);
-});
+  // Modal div must not be inside any element with relative or fixed postition
+  // or it will be shown behind it's backdrop. Let's move it to the top level of the body.
+  var modal = $('#chb-confirm-guid-modal')
+  $('body').append(modal)
+})
