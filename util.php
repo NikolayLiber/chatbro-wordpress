f5f7b2752d24811ec2e433415ecd0cd866ac63ea
Nikolay Liber <fwnd80@gmail.com>
2017-02-13 16:20:53 +0500

    Integration with WP User Avatar plugin.

--- util.php
+++ util.php
@@ -169,23 +169,6 @@ if (!class_exists("ChatBroUtils")) {
             return $page_match;
         }
 
-        public static function get_avatar_url()
-        {
-            $user = wp_get_current_user();
-            $site_user_avatar_url = "";
-            preg_match("/src='(.*)' alt/i", get_avatar($user->ID, 120), $avatar_path);
-
-            if(count($avatar_path)!=0)
-                $site_user_avatar_url = $avatar_path[1];
-
-            if($site_user_avatar_url == "")
-                $site_user_avatar_url = get_avatar_url($user->ID);
-
-            $site_user_avatar_url = strpos($site_user_avatar_url, 'wp_user_avatar') == FALSE ? $site_user_avatar_url : '';
-
-            return $site_user_avatar_url;
-        }
-
         public static function get_profile_url()
         {
             $user = wp_get_current_user();
